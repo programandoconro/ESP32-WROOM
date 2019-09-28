f2660ccead11274f1a219aaf4877dc06ef43b5db
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-09-28 13:41:12 +0100

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,4 +1,7 @@
 import network
+from machine import Pin
+from time import sleep
+import random
 
 ap= network.WLAN(network.AP_IF)
 ap.active(True)
@@ -8,24 +11,22 @@ sta = network.WLAN(network.STA_IF)
 sta.active(True)
 sta.connect("AndroidAP", "xjnn4112")
 sleep(1)
-sta.isconnected()
-sta.ifconfig()
 
-#('192.168.43.175', '255.255.255.0', '192.168.43.1', '192.168.43.1')
-
-##### random blinking #######
-
-
-while True:
-  sta.isconnected()==True
-  from machine import Pin
-  from time import sleep
-  import random
-
-  led = Pin(2, Pin.OUT)
-  for i in range (1,10**100):
-      r=random.randint(0,1)
-      sleep(0.1);led.value(r)
+if sta.isconnected()==True:
+    led = Pin(2, Pin.OUT)
+    for i in range (1,10**100):
+        r=random.randint(0,1)
+        sleep(0.1);led.value(r)
+else:        
+      led.value(0)
+    
+    
+    
+    
+    
+    
+    
       
       
+
       
