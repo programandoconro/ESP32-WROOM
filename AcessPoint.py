10819664959ef357e9b721e978cb66fbd8331110
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-10-11 02:04:54 +0100

    Update AcessPoint.py

--- AcessPoint.py
+++ AcessPoint.py
@@ -1,20 +1,11 @@
-from machine import Pin
-from time import sleep
-import random
-import network
-import esp
-import gc
 try:
   import usocket as socket
 except:
   import socket
-
-led = Pin(2, Pin.OUT)
-for i in range (1,10**100):
-    r=random.randint(0,1)
-    sleep(0.1);led.value(r)
-
+import network
+import esp
 esp.osdebug(None)
+import gc
 gc.collect()
 
 ssid = 'MicroPython-AP'
@@ -27,20 +18,31 @@ ap.config(essid=ssid, password=password)
 while ap.active() == False:
   pass
 
+print('Connection successful')
+print(ap.ifconfig())
+
 def web_page():
   html = """<html><head><meta name="viewport" content="width=device-width, initial-scale=1"></head>
   <body><h1>Hello, Hackers!</h1></body></html>"""
   return html
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 80))
 s.listen(5)
 
-while True:
-  conn, addr = s.accept()
-  print('Got a connection from %s' % str(addr))
-  request = conn.recv(1024)
-  print('Content = %s' % str(request))
-  response = web_page()
-  conn.send(response)
-  conn.close()
+conn, addr = s.accept()
+print('Got a connection from %s' % str(addr))
+request = conn.recv(1024)
+print('Content = %s' % str(request))
+response = web_page()
+conn.send(response)
+
+from machine import Pin
+from time import sleep
+import random
+
+led = Pin(2, Pin.OUT)
+for i in range (1,10**100):
+    r=random.randint(0,1)
+    sleep(0.1);led.value(r)
+
+
