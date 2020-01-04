91a7794ee6836bf75b7250349bb239333cf3ff5e
Ro <50117686+progamandoconro@users.noreply.github.com>
2020-01-04 21:39:03 +0000

    Update index.py

--- index.py
+++ index.py
@@ -1,27 +1,27 @@
-html = """
 <html>
 	<script>const myIndex = () => {alert('Hello Hackers')}</script>
 	<head>
 		<title> Hello Hackers !!</title>
 	</head>
 	<body>
-		<h5>Welcome to my site, please hack it and share me how.</h5>
+		<h1>Welcome to my site, please hack it and share me how.</h1>
 		<button onclick="myIndex()"> Push me </button>
 		<input> </input>
+		<hr />
+
+		<iframe src="https://progamandoconro.github.io/FibonacciMathGame/" height="2100" width="1200"></iframe>
+		<hr />
 		<iframe
-			src="https://progamandoconro.github.io/Image-Classification-ML-App/"
+			src="https://progamandoconro.github.io/Firebase-Real-Time-Web-Admin-App/"
 			height="1000"
 			width="1200"
 		></iframe>
 		<hr />
-		<iframe 
-         src="https://progamandoconro.github.io/FibonacciMathGame/" 
-         height="1000" 
-         width="1200"
-         ></iframe>
+		<iframe
+			src="https://progamandoconro.github.io/Image-Classification-ML-App/"
+			height="2100"
+			width="2000"
+		></iframe>
 		<hr />
 	</body>
-</html>;
-
-
-"""
+</html>
