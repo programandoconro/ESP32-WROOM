71a81d15531b71d99b992e2b9220ee4495b06623
Ro <50117686+progamandoconro@users.noreply.github.com>
2020-01-04 21:10:53 +0000

    Update index.py

--- index.py
+++ index.py
@@ -1,29 +1,27 @@
 html = """
-<!DOCTYPE html>
 <html>
-<script> 
-
-
-const myIndexHello = ()=>{
-   alert('Hello Hackers');    
-}
-
- </script>
-    <head> <title> Hello Hackers !!
-</title> </head>
-    <body> <h5>Welcome to my site, please hack it and share me how. 
-    </h5> 
-    <button onclick="myIndexHello()"> Push me </button>
-    <input> </input>
-
-<iframe 
-src="https://progamandoconro.github.io/Image-Classification-ML-App/" 
-height="400" width="600"
-></iframe> 
-   
-    </body>
-
-</html>
+	<script>const myIndex = () => {alert('Hello Hackers')}</script>
+	<head>
+		<title> Hello Hackers !!</title>
+	</head>
+	<body>
+		<h5>Welcome to my site, please hack it and share me how.</h5>
+		<button onclick="myIndex()"> Push me </button>
+		<input> </input>
+		<iframe
+			src="https://progamandoconro.github.io/Image-Classification-ML-App/"
+			height="1000"
+			width="1200"
+		></iframe>
+		<hr />
+		<iframe 
+         src="https://progamandoconro.github.io/FibonacciMathGame/" 
+         height="1000" 
+         width="1200"
+         ></iframe>
+		<hr />
+	</body>
+</html>;
 
 
 """
