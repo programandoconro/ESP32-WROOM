e2b682d593bc18d763c3d2b52a1ad19e8043bd06
Ro <50117686+progamandoconro@users.noreply.github.com>
2020-01-04 20:07:09 +0000

    Create index.py

---
+++ index.py
@@ -0,0 +1,29 @@
+html = """
+<!DOCTYPE html>
+<html>
+<script> 
+
+
+const myIndexHello = ()=>{
+   alert('Hello Hackers');    
+}
+
+ </script>
+    <head> <title> Hello Hackers !!
+</title> </head>
+    <body> <h5>Welcome to my site, please hack it and share me how. 
+    </h5> 
+    <button onclick="myIndexHello()"> Push me </button>
+    <input> </input>
+
+<iframe 
+src="https://progamandoconro.github.io/Image-Classification-ML-App/" 
+height="400" width="600"
+></iframe> 
+   
+    </body>
+
+</html>
+
+
+"""
