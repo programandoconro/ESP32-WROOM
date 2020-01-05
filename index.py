64f6f3c0d0f90c04b73dcf843f63cc88a02131c5
Ro <50117686+progamandoconro@users.noreply.github.com>
2020-01-05 01:47:49 +0000

    Update index.py

--- index.py
+++ index.py
@@ -8,29 +8,54 @@ html = """
 			<h1>Welcome to my site, please hack it and share me how.</h1>
 			<input onChange="myName(this.value)"> </input>
 			<button onclick="myIndex()"> Push me </button>
-			<br />
+			<hr />
 			<iframe
 				src="https://progamandoconro.github.io/Image-Classification-ML-App/"
 				height="2100"
 				width="2000"
 			></iframe>
-			<br />
+			<hr />
+			<iframe src="https://programandoconro.shinyapps.io/ANN_forest/" height="1000" width="1200"></iframe>
+			<hr />
+			<hr />
+			<iframe src="https://omgonzalez.shinyapps.io/carbon-prediction/" height="1000" width="1200"></iframe>
+			<hr />
+			<iframe src="https://omgonzalez.shinyapps.io/unsupervised_ml_coal/" height="1000" width="1200"></iframe>
+			<hr />
+			<iframe
+				src="https://programandoconro2.shinyapps.io/future_precipitation/"
+				height="1000"
+				width="2000"
+			></iframe>
+			<hr />
+			<iframe src="https://biorod.shinyapps.io/agentes_epidemia/" height="1000" width="2000"></iframe>
+			<hr />
+			<iframe src="https://rodlupanow.shinyapps.io/range/" height="1000" width="2000"></iframe>
+			<hr />
+			<iframe
+				src="https://programandoconro.shinyapps.io/rainforest_climate_change/"
+				height="1000"
+				width="2000"
+			></iframe>
 			<iframe src="https://progamandoconro.github.io/MyReactTODOApp/" height="2100" width="2000"></iframe>
-			<br />
-			<iframe src="https://programandoconro.wordpress.com/" height="2100" width="2000"></iframe>
-			<br />
+			<hr />
+			<hr />
 			<iframe src="https://progamandoconro.github.io/FibonacciMathGame/" height="2100" width="1200"></iframe>
-			<br />
+			<hr />
 			<iframe
 				src="https://progamandoconro.github.io/Firebase-Real-Time-Web-Admin-App/"
 				height="1000"
 				width="1200"
 			></iframe>
-			<br />
+			<hr />
+			<iframe src="https://programandoconro.wordpress.com/" height="2100" width="2000"></iframe>
+			<hr />
 		</body>
 	</center>
 	<script>const myIndex = () => {alert('Hello Hackers')}</script>
 	<script>const myName = (e) => {alert(e)}</script>
-</div>
+</div>;
+
+
 
 """
