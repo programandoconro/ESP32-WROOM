d678b20a7bd3835373a672d5c6615f9e734b0394
Ro <50117686+progamandoconro@users.noreply.github.com>
2020-01-05 00:34:55 +0000

    Update index.py

--- index.py
+++ index.py
@@ -1,51 +1,36 @@
 html = """
-
 <div>
-<center>
+	<center>
+		<head>
+			<title> Hello Hackers !!</title>
+		</head>
+		<body>
+			<h1>Welcome to my site, please hack it and share me how.</h1>
+			<input onChange="myName(this.value)"> </input>
+			<button onclick="myIndex()"> Push me </button>
+			<br />
+			<iframe
+				src="https://progamandoconro.github.io/Image-Classification-ML-App/"
+				height="2100"
+				width="2000"
+			></iframe>
+			<br />
+			<iframe src="https://progamandoconro.github.io/MyReactTODOApp/" height="2100" width="2000"></iframe>
+			<br />
+			<iframe src="https://programandoconro.wordpress.com/" height="2100" width="2000"></iframe>
+			<br />
+			<iframe src="https://progamandoconro.github.io/FibonacciMathGame/" height="2100" width="1200"></iframe>
+			<br />
+			<iframe
+				src="https://progamandoconro.github.io/Firebase-Real-Time-Web-Admin-App/"
+				height="1000"
+				width="1200"
+			></iframe>
+			<br />
+		</body>
+	</center>
 	<script>const myIndex = () => {alert('Hello Hackers')}</script>
 	<script>const myName = (e) => {alert(e)}</script>
-	
-	<head>
-		<title> Hello Hackers !!</title>
-	</head>
-	<body>
-		<h1>Welcome to my site, please hack it and share me how.</h1>
-		<input onChange="myName(this.value)"> </input>
-		<button onclick="myIndex()"> Push me </button>
-		<hr />
-
-		<iframe
-			src="https://progamandoconro.github.io/Image-Classification-ML-App/"
-			height="2100"
-			width="2000"
-		></iframe>
-		<hr />
-<iframe
-			src="https://progamandoconro.github.io/MyReactTODOApp/"
-			height="2100"
-			width="2000"
-		></iframe>
-		<hr />
-<iframe
-			src="https://programandoconro.wordpress.com/"
-			height="2100"
-			width="2000"
-		></iframe>
-		<hr />
-
-		<iframe src="https://progamandoconro.github.io/FibonacciMathGame/" height="2100" width="1200"></iframe>
-		<hr />
-		<iframe
-			src="https://progamandoconro.github.io/Firebase-Real-Time-Web-Admin-App/"
-			height="1000"
-			width="1200"
-		></iframe>
-		<hr />
-
-
-
-	</body>
-</center>
-</div>;
+</div>
 
 """
